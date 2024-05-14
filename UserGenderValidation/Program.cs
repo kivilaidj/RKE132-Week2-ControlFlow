588d0845933d4bb4b310348bbfbc71734af57c5d
janek <janek.kivilaid@tktk.ee>
2024-05-14 23:06:20 +0300

    week 2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Please, select your gender (m/F):");
+
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
